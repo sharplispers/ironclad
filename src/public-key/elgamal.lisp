f45a79c2ff63114fd696dcd7e5aec01606102784
Guillaume LE VAILLANT <guillaume.le.vaillant@openmailbox.org>
2015-11-24 16:08:03 +0100

    Code cleanup.

--- src/public-key/elgamal.lisp
+++ src/public-key/elgamal.lisp
@@ -16,14 +16,6 @@
   ((y :initarg :y :reader elgamal-key-y :type integer)
    (x :initarg :x :reader elgamal-key-x :type integer)))
 
-(defclass elgamal-ciphertext ()
-  ((c1 :initarg :c1 :reader elgamal-ciphertext-c1)
-   (c2 :initarg :c2 :reader elgamal-ciphertext-c2)))
-
-;; (defclass elgamal-signature ()
-;;   ((r :initarg :r :reader elgamal-signature-r)
-;;    (s :initarg :s :reader elgamal-signature-s)))
-
 (defun elgamal-key-p (elgamal-key)
   (group-pval (group elgamal-key)))
 
@@ -36,7 +28,6 @@
 (defmethod make-public-key ((kind (eql :elgamal))
                             &key p g y &allow-other-keys)
   (unless (and p g y)
-    ;; FIXME: "real" ironclad error needed here
     (error "Must specify all members of the DL group for ELGAMAL"))
   (let ((group (make-instance 'discrete-logarithm-group :p p :g g)))
     (make-instance 'elgamal-public-key :group group :y y)))
@@ -44,7 +35,6 @@
 (defmethod make-private-key ((kind (eql :elgamal))
                              &key p g y x &allow-other-keys)
   (unless (and p g y x)
-    ;; FIXME: "real" ironclad error needed here
     (error "Must specify all members of the DL group for ELGAMAL"))
   (let ((group (make-instance 'discrete-logarithm-group :p p :g g)))
     (make-instance 'elgamal-private-key :group group :y y :x x)))
@@ -58,16 +48,6 @@
     (values (make-private-key :elgamal :p p :g g :y y :x x)
             (make-public-key :elgamal :p p :g g :y y))))
 
-(defun make-elgamal-ciphertext (c1 c2)
-  (make-instance 'elgamal-ciphertext
-                 :c1 (maybe-integerize c1)
-                 :c2 (maybe-integerize c2)))
-
-;; (defun make-elgamal-signature (r s)
-;;   (make-instance 'elgamal-signature
-;;                  :r (maybe-integerize r)
-;;                  :s (maybe-integerize s)))
-
 (defun elgamal-generate-k (p)
   "Generate a random number K so that 0 < K < P - 1, and K is relatively prime to P - 1."
   (assert (> p 3))
@@ -77,22 +57,31 @@
        until (= 1 (gcd k (- p 1)))
        finally (return k))))
 
+;; TODO: integer-to-octets as big endian or little endian?
 (defun elgamal-encrypt (msg key)
-  (let* ((p (elgamal-key-p key))
+  (let* ((m (octets-to-integer msg))
+         (p (elgamal-key-p key))
+         (pbits (integer-length p))
          (g (elgamal-key-g key))
          (y (elgamal-key-y key))
          (k (elgamal-generate-k p))
          (c1 (expt-mod g k p))
-         (c2 (mod (* msg (expt-mod y k p)) p)))
-    (make-elgamal-ciphertext c1 c2)))
+         (c2 (mod (* m (expt-mod y k p)) p)))
+    (unless (< m p)
+      (error "Message can't be bigger than the order of the DL group"))
+    (concatenate '(simple-array (unsigned-byte 8) (*))
+                 (integer-to-octets c1 :n-bits pbits)
+                 (integer-to-octets c2 :n-bits pbits))))
 
 (defun elgamal-decrypt (ciphertext key)
   (let* ((p (elgamal-key-p key))
+         (pbits (integer-length p))
          (g (elgamal-key-g key))
          (x (elgamal-key-x key))
-         (c1 (elgamal-ciphertext-c1 ciphertext))
-         (c2 (elgamal-ciphertext-c2 ciphertext)))
-    (mod (* c2 (modular-inverse (expt-mod c1 x p) p)) p)))
+         (c1 (octets-to-integer (subseq ciphertext 0 (/ pbits 8))))
+         (c2 (octets-to-integer (subseq ciphertext (/ pbits 8) (/ pbits 4))))
+         (m (mod (* c2 (modular-inverse (expt-mod c1 x p) p)) p)))
+    (integer-to-octets m)))
 
 (defmethod encrypt-message ((key elgamal-private-key) msg &key (start 0) end)
   (let ((public-key (make-public-key :elgamal
@@ -101,38 +90,21 @@
                                      :y (elgamal-key-y key))))
     (encrypt-message public-key msg :start start :end end)))
 
-;; TODO: integer-to-octets as big endian or little endian?
 (defmethod encrypt-message ((key elgamal-public-key) msg &key (start 0) end)
-  (let ((m (octets-to-integer msg :start start :end end))
-        (p (elgamal-key-p key)))
-    (unless (< m p)
-      ;; FIXME: "real" ironclad error needed here
-      (error "Message can't be bigger than the order of the DL group"))
-    (let ((n (integer-length p))
-          (c (elgamal-encrypt m key)))
-      (concatenate '(simple-array (unsigned-byte 8) (*))
-                   (integer-to-octets (elgamal-ciphertext-c1 c) :n-bits n)
-                   (integer-to-octets (elgamal-ciphertext-c2 c) :n-bits n)))))
+  (elgamal-encrypt (subseq msg start end) key))
 
 (defmethod decrypt-message ((key elgamal-private-key) msg &key (start 0) end)
   (let* ((p (elgamal-key-p key))
-         (g (elgamal-key-g key))
-         (x (elgamal-key-x key))
          (end (or end (length msg))))
-    (unless (= (- end start) (* 2 (ceiling (integer-length p) 8)))
-      ;; FIXME: "real" ironclad error needed here
-      (error "Ciphertext size must be twice the order of the DL group"))
-    (let* ((half-length (/ (- end start) 2))
-           (c1 (octets-to-integer msg :start start :end half-length))
-           (c2 (octets-to-integer msg :start half-length :end end))
-           (c (make-elgamal-ciphertext c1 c2)))
-      (integer-to-octets (elgamal-decrypt c key)))))
+    (unless (= (* 4 (- end start)) (integer-length p))
+      (error "Bad ciphertext length"))
+    (elgamal-decrypt (subseq msg start end) key)))
 
 (defmethod sign-message ((key elgamal-private-key) msg &key (start 0) end)
   (let* ((m (octets-to-integer msg :start start :end end))
-         (p (elgamal-key-p key)))
+         (p (elgamal-key-p key))
+         (pbits (integer-length p)))
     (unless (< m (- p 1))
-      ;; FIXME: "real" ironclad error needed here
       (error "Message can't be bigger than the order of the DL group minus 1"))
     (let* ((g (elgamal-key-g key))
            (x (elgamal-key-x key))
@@ -141,20 +113,23 @@
            (s (mod (* (- m (* r x)) (modular-inverse k (- p 1))) (- p 1))))
       (if (not (zerop s))
           (concatenate '(simple-array (unsigned-byte 8) (*))
-                       (integer-to-octets r :n-bits (integer-length p))
-                       (integer-to-octets s :n-bits (integer-length p)))
+                       (integer-to-octets r :n-bits pbits)
+                       (integer-to-octets s :n-bits pbits))
           (sign-message key msg :start start :end end)))))
 
 (defmethod verify-signature ((key elgamal-public-key) msg signature &key (start 0) end)
   (let* ((m (octets-to-integer msg :start start :end end))
-         (p (elgamal-key-p key)))
+         (p (elgamal-key-p key))
+         (pbits (integer-length p)))
+    (unless (= (* 4 (length signature)) pbits)
+      (error "Bad signature length"))
     (unless (< m (- p 1))
       ;; FIXME: "real" ironclad error needed here
       (error "Message can't be bigger than the order of the DL group minus 1"))
     (let* ((g (elgamal-key-g key))
            (y (elgamal-key-y key))
-           (r (octets-to-integer (subseq signature 0 (/ (integer-length p) 8))))
-           (s (octets-to-integer (subseq  signature (/ (integer-length p) 8) (/ (integer-length p) 4)))))
+           (r (octets-to-integer (subseq signature 0 (/ pbits 8))))
+           (s (octets-to-integer (subseq  signature (/ pbits 8) (/ pbits 4)))))
       (and (< 0 r p)
            (< 0 s (- p 1))
            (= (expt-mod g m p)
