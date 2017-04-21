d3134a6df1c89398b91f7b46f4c70270a72d1f68
Robert A. Uhl <bob.denver.co@gmail.com>
2017-04-21 06:21:54 -0600

    Add test of (MAKE-PRNG :FORTUNA :SEED URANDOM)

--- testing/test-vectors/prng-tests.lisp
+++ testing/test-vectors/prng-tests.lisp
@@ -2,5 +2,8 @@
 (in-package :crypto-tests)
 
 (rtest:deftest :prng-fortuna (run-test-vector-file :prng *prng-tests*) t)
+(rtest:deftest :prng-fortuna-urandom (let ((prng (crypto:make-prng :fortuna :seed urandom)))
+                                       (= (length (crypto:random-data 16 prng))
+                                          16)) t)
 
 ;;    (random-data (make-prng :fortuna :seed (coerce #(0) 'simple-octet-vector)) 1) #(28))
